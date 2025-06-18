a471f4224a2bafd540da26596717aac70614c86a
Rudish Thapa <thaparudish07@gmail.com>
2025-06-18 14:14:41 +0545

    profile controller nav

--- src/eventflow/controllers/ProfileController.java
+++ src/eventflow/controllers/ProfileController.java
@@ -10,30 +10,42 @@ import eventflow.views.Dashboard;
 import eventflow.views.MyEvents;
 import eventflow.views.MyTickets;
 import eventflow.views.ProfileForm;
+import eventflow.views.WalletForm;
 
 /**
  *
  * @author kisma
  */
 public class ProfileController {
-        public static void goToCreate(User user) {
-        Create createPage = new Create(user);
-        createPage.setVisible(true);
-    }
-    public static void goToMyEvents(User user) {
-    MyEvents myEventsPage = new MyEvents(user);
-    myEventsPage.setVisible(true);
-}
-public static void goToMyTickets(User user) {
-    MyTickets myTicketsPage = new MyTickets(user);
-    myTicketsPage.setVisible(true);
-}
-public static void goToProfile(User user) {
-    ProfileForm profilePage = new ProfileForm(user);
-    profilePage.setVisible(true);
-}
- public static void goToDashboard(User user) {
+        public static void goToDashboard(User user){
+        
         Dashboard dashboardPage = new Dashboard(user);
         dashboardPage.setVisible(true);
     }
+    public static void goToCreate(User user) {
+        Create createPage = new Create(user);
+        createPage.setVisible(true);
+    }
+
+    public static void goToMyEvents(User user) {
+        MyEvents myEventsPage = new MyEvents(user);
+        myEventsPage.setVisible(true);
+    }
+
+    public static void goToMyTickets(User user) {
+        MyTickets myTicketsPage = new MyTickets(user);
+        myTicketsPage.setVisible(true);
+    }
+
+    public static void goToProfile(User user) {
+        ProfileForm profilePage = new ProfileForm(user);
+        profilePage.setVisible(true);
+    }
+    
+    public static void goToWallet(User user) {
+    WalletForm walletPage = new WalletForm(user);
+    walletPage.setVisible(true);
+}
+ 
+ 
 }
