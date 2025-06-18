fd6e889aec5729415a187ebcb67d6341d687655a
manjilthapa0777 <manjilt751@gmail.com>
2025-06-18 14:37:57 +0545

    eroror fix

--- src/eventflow/views/ChangePasswordForm.java
+++ src/eventflow/views/ChangePasswordForm.java
@@ -303,11 +303,7 @@ public class ChangePasswordForm extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ChangePasswordForm().setVisible(true);
-            }
-        });
+      
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
