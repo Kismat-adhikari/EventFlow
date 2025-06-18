eab361e83e87827b1c9f35f7625a66305366348e
Rudish Thapa <thaparudish07@gmail.com>
2025-06-18 14:11:48 +0545

    Tickets controller

--- src/eventflow/controllers/TicketController.java
+++ src/eventflow/controllers/TicketController.java
@@ -8,26 +8,7 @@ import eventflow.views.MyTickets;
 import eventflow.views.ProfileForm;
 
 public class TicketController {
-     public static void goToCreate(User user) {
-        Create createPage = new Create(user);
-        createPage.setVisible(true);
-    }
-    public static void goToMyEvents(User user) {
-    MyEvents myEventsPage = new MyEvents(user);
-    myEventsPage.setVisible(true);
-}
-public static void goToMyTickets(User user) {
-    MyTickets myTicketsPage = new MyTickets(user);
-    myTicketsPage.setVisible(true);
-}
-public static void goToProfile(User user) {
-    ProfileForm profilePage = new ProfileForm(user);
-    profilePage.setVisible(true);
-}
- public static void goToDashboard(User user) {
-        Dashboard dashboardPage = new Dashboard(user);
-        dashboardPage.setVisible(true);
-    }
+    
  
  
  public static double getUserBalance(User user) {
